8ce5946140ddcd36f91e07f717a73584df5611ef
Alejandro Sainz <alejandro.sainz.m@hotmail.com>
2022-08-07 18:57:27 -0500

    Last Commit

---
+++ app.js
@@ -0,0 +1,12 @@
+
+var a = 10;
+var b = 20;
+var c = a + b;
+console.log(c); 
+
+let mypersona1 = new Persona('Alejandro', 'Perez', 30, 1.9);
+let mypersona2 = new Persona('Alejandro', 'Juarez', 30, 1.7);
+console.log(mypersona1.saludar());
+console.log(mypersona2.saludar());
+
+
